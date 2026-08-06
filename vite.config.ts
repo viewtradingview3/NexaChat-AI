0e1f7e7e6bb7b0dea09ae513f889d518f26463c5
viewtradingview3 <viewtradingview3@gmail.com>
2026-08-06 13:02:59 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: "0.0.0.0",
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8787",
-        changeOrigin: true
-      }
-    }
+    allowedHosts: true,
   },
-  preview: {
-    host: "0.0.0.0"
-  }
 });
